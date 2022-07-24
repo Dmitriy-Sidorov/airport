47dc49c3bde0c49e082cd6ba856c9ed1700f0de8
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2022-07-24 16:13:13 +0300

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {MainPage} from "./pages/MainPage";
+import {AuthPage} from "./pages/AuthPage";
+import {AirportDetailsPage} from "./pages/AirportDetailsPage";
 
 function App() {
   return (
-    <div>Test</div>
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="/auth" element={<AuthPage/>}/>
+        <Route path="/airport/:id" element={<AirportDetailsPage/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/AirportDetailsPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function AirportDetailsPage() {
+    return (
+        <div>AirportDetailsPage</div>
+    )
+}
