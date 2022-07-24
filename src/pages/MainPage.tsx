67c6e39eb4e90c5ba76a2cb8936903fc5bf5fb96
Dmitriy Sidorov <dmitrii.sidorov@accenture.com>
2022-07-24 16:46:44 +0300

    inject components

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react'
+import { AirportSearch } from '../components/AirportSearch'
+import { AirportFilter } from '../components/AirportFilter'
+import { AirportCard } from '../components/AirportCard'
 
 export function MainPage() {
-    return (
-        <div>main</div>
-    )
+	return (
+		<div className="container mx-auto p-4 flex justify-center">
+			<div>
+				<AirportSearch/>
+				<AirportFilter/>
+
+				<div className="min-w-[760px]">
+					<p className="text-center">Loading...</p>
+
+					<AirportCard/>
+				</div>
+			</div>
+		</div>
+	)
 }
